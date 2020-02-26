0a31ce2f9e9ccd389bb16b6e19978988c6dd8d50
CJ Roberts <cjroberts2013@gmail.com>
2020-02-25 22:17:06 -0600

    Created the AuthorType object and associated it with the booktype throught the use of the authorid property of the booktype

--- schema/schema.js
+++ schema/schema.js
@@ -1,13 +1,40 @@
 const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLSchema,
+	GraphQLInt,
+	GraphQLList
+} = graphql;
 
 const BookType = new GraphQLObjectType({
 	name: "Book",
 	fields: () => ({
 		id: { type: GraphQLString },
 		name: { type: GraphQLString },
-		genre: { type: GraphQLString }
+		genre: { type: GraphQLString },
+		author: {
+			type: AuthorType,
+			resolve(parentValue, args) {
+				return _.find(authors, { id: parentValue.authorId });
+			}
+		}
+	})
+});
+
+const AuthorType = new GraphQLObjectType({
+	name: "Author",
+	fields: () => ({
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		age: { type: GraphQLInt },
+		books: {
+			type: new GraphQLList(BookType),
+			resolve(parentValue, args) {
+				return _.filter(books, { authorId: parentValue.id });
+			}
+		}
 	})
 });
 
@@ -18,6 +45,25 @@ const RootQuery = new GraphQLObjectType({
 			type: BookType,
 			args: { id: { type: GraphQLString } },
 			resolve(parentValue, args) {}
+		},
+		author: {
+			type: AuthorType,
+			args: { id: { type: GraphQLString } },
+			resolve(parentValue, args) {
+				return;
+			}
+		},
+		books: {
+			type: new GraphQLList(BookType),
+			resolve(parentValue, args) {
+				return books;
+			}
+		},
+		authors: {
+			type: new GraphQLList(AuthorType),
+			resolve(parentValue, args) {
+				return authors;
+			}
 		}
 	}
 });
