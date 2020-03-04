135190428aa0958706d79d67698410acac81839f
CJ Roberts <cjroberts2013@gmail.com>
2020-03-03 23:53:04 -0600

    Completed functionality of the project. Created a book details component that takes the information about the book, and its author and displays it below the list of books

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
-import { getAuthorsQuery } from "./../queries/queries";
+import {
+	getAuthorsQuery,
+	addBookMutation,
+	getBooksQuery
+} from "./../queries/queries";
+import * as compose from "lodash.flowright";
 
 class AddBook extends Component {
 	constructor(props) {
@@ -13,7 +18,7 @@ class AddBook extends Component {
 	}
 
 	displayAuthors() {
-		let data = this.props.data;
+		let data = this.props.getAuthorsQuery;
 		if (data.loading) {
 			return <option disabled>Loading Authors...</option>;
 		} else {
@@ -29,7 +34,14 @@ class AddBook extends Component {
 
 	submitForm(e) {
 		e.preventDefault();
-		console.log(this.state);
+		this.props.addBookMutation({
+			variables: {
+				name: this.state.name,
+				genre: this.state.genre,
+				authorId: this.state.authorId
+			},
+			refetchQueries: [{ query: getBooksQuery }]
+		});
 	}
 
 	render() {
@@ -65,4 +77,7 @@ class AddBook extends Component {
 	}
 }
 
-export default graphql(getAuthorsQuery)(AddBook);
+export default compose(
+	graphql(getAuthorsQuery, { name: "getAuthorsQuery" }),
+	graphql(addBookMutation, { name: "addBookMutation" })
+)(AddBook);
